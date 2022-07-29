7b2f00867710981171194e60ce3513f1581fdccb
arikatia <109274970+arikatia@users.noreply.github.com>
2022-07-29 10:32:03 +0100

    Update Binarysearch.asm

--- Binarysearch.asm
+++ Binarysearch.asm
@@ -1,64 +1,55 @@
-;search in a sorted array by the binary search method
-;input: SI = array offset address
-;       BX = number of elements
-;       CX = key
-;output: SI = offset of sorted array
-;        AX = pos @where key has been found
-    ;PUSH AX
-    PUSH BX
-    PUSH CX
-    PUSH DX
-    PUSH SI
-    PUSH DI
-    
-    
-    MOV DX,BX   ; DX = rIndex
-    DEC DX 
-    MOV BX,0    ; BX = lIndex
+.model small                           ; assembler directive to allocate memory
 
+.data                                  ; data segment
+    arr dw 0, 1, 2, 3, 4, 5, 6, 7, 8, 9      ; array of 10 16-bit numbers
+    len dw ($-arr)/2                   ; length of the array
+    key equ 8                          ; key to be searched
+    msg1 db "key is found at "         ; message 1 to print if key is found
+    res db "  position $"              ; message 2 to print if key is found
+    msg2 db 'key is not found!!!.$'    ; message to print if key is not found
 
-    @START_BIN_SEARCH: ; BX=l,AX=m,DX=r
-        CMP BX,DX ; exit when lIndex > rIndex
-        JG @NOT_FOUND_BIN_SEARCH
-        MOV AX,BX
-        ADD AX,DX ; AX=BX+DX
-        SHR AX,1 ; AX = midIndex ; m=(l+r)/2
-        
-        ;MOV SI,AX
-        ;ADD SI,SI
-        MOV DI,SI
-        ADD DI,AX
-        ADD DI,AX
-        CMP CX,[DI]
-        JE @FOUND_BIN_SEARCH            
-        JG @BIG_PIVOT_BIN_SEARCH
-        JMP @SMALL_PIVOT_BIN_SEARCH
+.code                    ; code segment
 
-        @BIG_PIVOT_BIN_SEARCH:
-            INC AX 
-            MOV BX,AX ; l=m+1
-            JMP @START_BIN_SEARCH
+     mov ax, @data       ; initialize ds register
+     mov ds, ax
+     mov bx, 00          ; bx = 0, lower bound 
+     mov dx, len         ; dx = len, upper bound
+     mov cx, key         ; cx = key to be searched
 
-        @SMALL_PIVOT_BIN_SEARCH:
-            DEC AX
-            MOV DX,AX ; r=m-1
-            JMP @START_BIN_SEARCH
+     searchloop:         ; label Searchloop starts
+        cmp bx, dx       ; compare bx with dx
+        ja fail          ; if bx > ax i.e. lower bound > upper bound, jump to label fail
+        mov ax, bx       ; move bx i.e. lower bound to ax
+        add ax, dx       ; add ax with dx, i.e. lower bound with upper bound and store result in ax
+        shr ax, 1        ; shift ax right by 1 to divide sum by 2
+        mov si, ax       ; move result in ax to si
+        add si, si       ; add si with si to point to mid element
+        cmp cx, arr[si]  ; compare cx with mid element
+        jae high         ; if cx >= mid element, jump to label high else continue
+        dec ax           ; decrement ax by one to get mid - 1
+        mov dx, ax       ; move dx to ax, change upper bound to mid - 1
+        jmp searchloop   ; start the searchloop again
 
+     high:               ; label big
+        je match         ; if mid element = cx, jump to match else continue
+        inc ax           ; increment ax by one to get mid + 1
+        mov bx, ax       ; move ax to bx, change lower bound to mid + 1
+        jmp searchloop   ; jump to label searchloop
 
-    @NOT_FOUND_BIN_SEARCH:
-        MOV AX,-1
-        JMP @END_BIN_SEARCH
+     match:              ; label Match
+        add al, 01       ; add 1 to al to obtain position of key in the array
+        add al, 30h      ; add 30 to convert it to integer
+        lea si, res      ; load effective address of res in si
+        mov [si], al     ; replace character at first location of res with al, i.e. position
+        lea dx, msg1     ; load effective address of msg1 in dx
+        jmp display      ; jump to label Display
 
-    @FOUND_BIN_SEARCH: ; index already in AX
-        ;ADD AL,01
-        JMP @END_BIN_SEARCH
+     fail:               ; label fail
+        lea dx, msg2     ; load effective address of msg2 in dx
 
-
-    @END_BIN_SEARCH:
-        POP DI
-        POP SI
-        POP DX
-        POP CX
-        POP BX
-        ;POP AX
-    RET
+     display:               ; label disp
+        mov ah, 09h      ; to print content present in dx
+        int 21h
+        mov ah, 4ch      ; terminate with return code
+        int 21h
+        end              ; end directive
